ea38b524dc068361f3dea58db7253c85f42a2855
Caleb Sattgast <calebsattgast@gmail.com>
2017-01-10 13:12:39 -0800

    build out employee model auth layers

--- model/employee.js
+++ model/employee.js
@@ -15,6 +15,7 @@ const employeeSchema = Schema({
   username: { type: String, unique: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
+  // storeID: { TODO: reference to store, build out later },
   admin: { type: Boolean, required: true },
   shipping: { type: Boolean },
   receiving: { type: Boolean }
@@ -28,3 +29,60 @@ employeeSchema.pre('save', function(next) {
   };
   next();
 });
+
+employeeSchema.methods.hashPassword = function(password) {
+  debug('hashPassword');
+
+  return new Promise((resolve, reject) => {
+    bcrypt.hash(password, 10, (err, hash) => {
+      if (err) return reject(err);
+      this.password = hash;
+      resolve(this);
+    });
+  });
+};
+
+employeeSchema.methods.validatePassword = function(password) {
+  debug('validatePassword');
+
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(password, this.password, (err, valid) => {
+      if (err) return reject(err);
+      if (!valid) return reject(createError(401, 'Wrong password'));
+      resolve(this);
+    });
+  });
+};
+
+employeeSchema.methods.generateFindHash = function() {
+  debug('generateFindHash');
+
+  return new Promise((resolve, reject) => {
+    let attempts = 0;
+
+    _generateFindHash.call(this);
+
+    function _generateFindHash() {
+      this.findHash = crypto.randomBytes(32).toString('hex');
+      this.save()
+      .then( () => resolve(this.findHash))
+      .catch( err => {
+        if (attempts > 3) return reject(err);
+        attempts++;
+        _generateFindHash.call(this);
+      });
+    };
+  });
+};
+
+employeeSchema.methods.generateToken = function() {
+  debug('generateToken');
+
+  return new Promise((resolve, reject) => {
+    this.generateFindHash()
+    .then( findHash => resolve(jwt.sign({ token: findHash }, process.env.APP_SECRET)))
+    .catch( err => reject(err));
+  });
+};
+
+module.exports = mongoose.module('employee', employeeSchema);
