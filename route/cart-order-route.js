dad684b1b7354b5d4a5bfbbac7f30801ececba8c
Steven Bateman <batemanvo@gmail.com>
2017-01-10 13:57:02 -0800

    Started writing tests, plus updates. Updated cart order POST route to specifically post to a particular customer.

--- route/cart-order-route.js
+++ route/cart-order-route.js
@@ -5,16 +5,28 @@ const parseJSON = require('body-parser').json();
 const createError = require('http-errors');
 const debug = require('debug')('inventory:cart order route');
 const CartOrder = require('../model/cart-order.js');
-// const Customer = require('../model/customer.js');
+const Customer = require('../model/customer.js');
 
 const cartOrderRouter = module.exports = Router();
 
-cartOrderRouter.post('/api/cartOrder', parseJSON, function(request, response, next) {
+cartOrderRouter.post('/api/orders/:customerID/cartOrder', parseJSON, function(request, response, next) {
   debug('POST: /api/cartOrder');
 
   if (Object.getOwnPropertyNames().length === 0) next(createError(400, 'No body posted.'));
 
-  new CartOrder(request.body).save()
-  .then(order => response.json(order))
+  Customer.findById(request.params.customerID)
+  .then(customer => {
+    if (!request.body.shippingAddress) request.body.shippingAddress = customer.address;
+    if (!request.body.shippingName) request.body.shippingName = customer.name;
+
+    this.tempCustomer = customer;
+    return new CartOrder(request.body).save();
+  })
+  .then(order => {
+    this.tempOrder = order;
+    this.tempCustomer.currentOrders.push(order._id);
+    return this.tempCustomer.save();
+  })
+  .then(() => response.json(this.tempOrder))
   .catch(next);
 });
--- test/cart-order-route-test.js
+++ test/cart-order-route-test.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const expect = require('chai').expect;
+const request = require('superagent');
+const CartOrder = require('../model/cart-order.js');
+const Customer = require('../model/customer.js');
+
+require('../server.js');
+
+const url = `http://localhost:${process.env.PORT}`;
+
+const exampleCustomer = {
+  name: 'Test user',
+  password: 'Testword',
+  email: 'test@test.com',
+  address: 'Test address',
+  username: 'Test username'
+};
+
+const exampleOrder = {
+  shippingAddress: exampleCustomer.address,
+  shippingName: exampleCustomer.name,
+};
