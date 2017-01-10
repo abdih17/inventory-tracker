6320e0421458f0703a0228e08b743b900b1d7cf1
Caleb Sattgast <calebsattgast@gmail.com>
2017-01-10 14:27:50 -0800

    set up employee auth router, build out POST route

--- route/employee-route.js
+++ route/employee-route.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// TODO: build out route
+const Router = require('express').Router;
+const jsonParser = require('body-parser').json();
+const createError = require('http-errors');
+const basicAuth = require('../lib/basic-auth-middleware.js');
+const debug = require('debug')('inventory:employeeRouter');
+
+const Employee = require('../model/employee.js');
+
+const employeeRouter = module.exports = Router();
+
+employeeRouter.post('/api/employee/register', jsonParser, function(req, res, next) {
+  debug('POST Employee: /api/employee/register');
+
+  if (Object.getOwnPropertyNames(req.body).length === 0) next(createError(400, 'No body included.'));
+
+  let password = req.body.password;
+  delete req.body.password;
+
+  let employee = new Employee(req.body);
+  console.log('***THIS IS THE EMPLOYEE***',employee);
+
+  employee.hashPassword(password)
+  .then( employee => employee.save())
+  .then( employee => employee.generateToken())
+  .then( token => {
+    console.log('***THIS IS THE TOKEN***', token);
+    res.status(201).send(token);
+  })
+  .catch(next);
+});
