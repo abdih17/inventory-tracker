0b6f56dde998ba563885090542acd43caf008c46
Caleb Sattgast <calebsattgast@gmail.com>
2017-01-11 10:53:03 -0800

    build out PUT routes for employees

--- route/employee-route.js
+++ route/employee-route.js
@@ -4,6 +4,7 @@ const Router = require('express').Router;
 const jsonParser = require('body-parser').json();
 const createError = require('http-errors');
 const basicAuth = require('../lib/basic-auth-middleware.js');
+const bearerAuth = require('../lib/bearer-auth-middleware.js');
 const debug = require('debug')('inventory:employeeRouter');
 
 const Employee = require('../model/employee.js');
@@ -47,4 +48,24 @@ employeeRouter.get('/api/employee/signin', basicAuth, function(req, res, next) {
     });
   })
   .catch(() => next(createError(401, 'Invalid login')));
+});
+
+employeeRouter.put('/api/employee/:employeeID', bearerAuth, jsonParser, function(req, res, next) {
+  debug('PUT Employee: /api/employee/:employeeID');
+
+  if (Object.getOwnPropertyNames(req.body).length === 0) next(createError(400, 'No body included.'));
+
+  Employee.findByIdAndUpdate(req.params.employeeID, req.body, {new: true})
+  .then(employee => {
+    if (req.body.password) {
+      let password = req.body.password;
+      delete req.body.password;
+      return employee.hashPassword(password)
+      .then(employee => employee.save())
+      .then(() => res.status(200).send('Update successful'))
+      .catch(() => next(createError(404, 'Employee not found.')));
+    }
+    res.status(200).send('Update successful');
+  })
+  .catch(() => next(createError(404, 'Employee not found.')));
 });
