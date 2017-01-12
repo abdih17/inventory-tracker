f69c168378a0e7b346214b053e87f363b5a0852c
Steven Bateman <batemanvo@gmail.com>
2017-01-11 21:24:11 -0800

    Changed store route. Populating the employees was showing the hashed passwords. Instead, the employees array is no longer populated so that passwords are not shown, and you have to specifically check each employee by logging in as that employee.

--- route/store-route.js
+++ route/store-route.js
@@ -25,7 +25,6 @@ storeRouter.get('/api/store/:id', function(req, res, next) {
   Store.findById(req.params.id)
   .populate('outgoing')
   .populate('incoming')
-  .populate('employees')
   .populate('current')
   .then( store => res.json(store))
   .catch( err => next(createError(404, err.message)));
